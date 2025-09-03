0f666472b701248e39748232d63d685f32703d3f
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-09-03 19:24:21 +0530

    c

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const urlRoute= require('./routes/url');
 const app= express();
 const PORT=8001;
 
+app.use(express.json());
+
+
 app.use("/url", urlRoute);
 
 connectToMongoDB('mongodb://localhost:27017/short-url').then(()=>{
